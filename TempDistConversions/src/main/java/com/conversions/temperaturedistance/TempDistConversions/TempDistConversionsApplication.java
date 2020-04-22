e7beeb0c7c9989545f9f780ff68588a82d30154d
SatishDoddapaneni <64103661+SatishDoddapaneni@users.noreply.github.com>
2020-04-22 08:36:49 +0200

    Add files via upload

---
+++ TempDistConversions/src/main/java/com/conversions/temperaturedistance/TempDistConversions/TempDistConversionsApplication.java
@@ -0,0 +1,13 @@
+package com.conversions.temperaturedistance.TempDistConversions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempDistConversionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempDistConversionsApplication.class, args);
+	}
+
+}
---
+++ TempDistConversions/src/test/java/com/conversions/temperaturedistance/TempDistConversions/TempDistConversionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversions.temperaturedistance.TempDistConversions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempDistConversionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
